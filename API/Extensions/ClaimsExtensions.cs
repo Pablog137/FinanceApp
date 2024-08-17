f1dfbc487396264112ac0873c81f6d97d3cdcfb9
Pablo <pablogutierrezvaldin@gmail.com>
2024-08-17 08:51:58 +0200

    Get user id extension method

---
+++ API/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsExtensions
+    {
+
+
+        public static int? GetUserId(this ClaimsPrincipal user)
+        {
+            var userIdClaim = user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (int.TryParse(userIdClaim, out int userId))
+            {
+                return userId;
+            }
+            return null;
+        }
+    }
+}
