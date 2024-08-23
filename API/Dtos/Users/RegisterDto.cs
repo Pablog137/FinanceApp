742e1fea07c5767d7982147719cbf585c7d6429b
Pablo <pablogutierrezvaldin@gmail.com>
2024-08-23 18:58:03 +0200

    Confirm password

--- API/Dtos/Users/RegisterDto.cs
+++ API/Dtos/Users/RegisterDto.cs
@@ -9,6 +9,8 @@ namespace API.Dtos.Users
     public class RegisterDto
     {
         [Required(ErrorMessage = "Username is required")]
+        [MinLength(3, ErrorMessage = "Username must be at least 3 characters long")]
+        [MaxLength(20, ErrorMessage = "Username must be at most 20 characters long")]
         public string Username { get; set; }
 
         [Required(ErrorMessage = "Email is required")]
@@ -17,5 +19,9 @@ namespace API.Dtos.Users
 
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
     }
 }
