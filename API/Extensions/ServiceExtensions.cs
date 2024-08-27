52a7c16ca9179efe356b4da21108cea56da4572a
Pablo <pablogutierrezvaldin@gmail.com>
2024-08-27 21:54:34 +0200

    Program file organized

---
+++ API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,130 @@
+﻿using API.Data;
+using API.Helpers;
+using API.Interfaces.Repositories;
+using API.Interfaces.Services;
+using API.Models;
+using API.Repository;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IContactService, ContactService>();
+            services.AddScoped<ITransactionService, TransactionService>();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<INotificationService, NotificationService>();
+            services.AddScoped<ITransferService, TransferService>();
+        }
+
+        public static void AddRepositories(this IServiceCollection services)
+        {
+            // Add repositories
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<IAccountRepository, AccountRepository>();
+            services.AddScoped<IContactRepository, ContactRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<INotificationRepository, NotificationRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+            services.AddScoped<ITransferRepository, TransferRepository>();
+        }
+
+        public static void AddJwtAuth(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme =
+                options.DefaultChallengeScheme =
+                options.DefaultScheme =
+                options.DefaultSignInScheme =
+                options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JWT:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:Audience"],
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SigningKey"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+
+                };
+
+            });
+
+        }
+
+        public static void AddIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(opt =>
+            {
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 2;
+                opt.Password.RequireDigit = false;
+                opt.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+
+        }
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(option =>
+            {
+                option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+        });
+            });
+        }
+
+       public static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options
+                .UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        public static void AddHealthCheck(this IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                .AddCheck<HealthCheck>("API and DB Check", tags: new[] { "api", "db" });
+        }
+
+
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,22 +1,11 @@
-using System.Text;
 using API.Data;
-using API.Helpers;
-using API.Interfaces.Repositories;
-using API.Interfaces.Services;
-using API.Models;
-using API.Repository;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using Serilog;
 
 
 try
 {
-
     var builder = WebApplication.CreateBuilder(args);
 
     Log.Logger = new LoggerConfiguration()
@@ -25,101 +14,16 @@ try
 
     Log.Information("Starting web application");
 
-
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
-
-    builder.Services.AddSwaggerGen(option =>
-    {
-        option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
-        option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Description = "Please enter a valid token",
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            BearerFormat = "JWT",
-            Scheme = "Bearer"
-        });
-        option.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                }
-            },
-            new string[]{}
-        }
-        });
-    });
-
-    builder.Services.AddDbContext<AppDbContext>(options => options
-        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-    builder.Services.AddIdentity<AppUser, AppRole>(opt =>
-    {
-        opt.Password.RequireUppercase = false;
-        opt.Password.RequireNonAlphanumeric = false;
-        opt.Password.RequiredLength = 2;
-        opt.Password.RequireDigit = false;
-        opt.User.RequireUniqueEmail = true;
-    }).AddEntityFrameworkStores<AppDbContext>()
-    .AddDefaultTokenProviders();
-
-    // JWT Authentication
-    builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme =
-        options.DefaultChallengeScheme =
-        options.DefaultScheme =
-        options.DefaultSignInScheme =
-        options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
-    }).AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = builder.Configuration["JWT:Issuer"],
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["JWT:Audience"],
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])),
-            ValidateLifetime = true,
-            ClockSkew = TimeSpan.Zero,
-
-        };
-
-    });
-
-    // Services
-    builder.Services.AddScoped<ITokenService, TokenService>();
-    builder.Services.AddScoped<IContactService, ContactService>();
-    builder.Services.AddScoped<ITransactionService, TransactionService>();
-    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-    builder.Services.AddScoped<INotificationService, NotificationService>();
-    builder.Services.AddScoped<ITransferService, TransferService>();
-
-    // Repositories
-    builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-    builder.Services.AddScoped<IAccountRepository, AccountRepository>();
-    builder.Services.AddScoped<IContactRepository, ContactRepository>();
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<INotificationRepository, NotificationRepository>();
-    builder.Services.AddScoped<ITokenRepository, TokenRepository>();
-    builder.Services.AddScoped<ITransferRepository, TransferRepository>();
-
-
-    // Logging
+    builder.Services.AddSwagger();
+    builder.Services.AddDbContext(builder.Configuration);
+    builder.Services.AddIdentity();
+    builder.Services.AddJwtAuth(builder.Configuration);
+    builder.Services.AddApplicationServices();
+    builder.Services.AddRepositories();
     builder.Services.AddSerilog();
-
-    builder.Services.AddHealthChecks()
-      .AddCheck<HealthCheck>("API and DB Check", tags: new[] { "api", "db" });
-
+    builder.Services.AddHealthCheck();
 
     var app = builder.Build();
 
@@ -138,8 +42,6 @@ try
         app.UseSwagger();
         app.UseSwaggerUI();
     }
-
-    app.UseHttpsRedirection();
 
     app.UseAuthentication();
     app.UseAuthorization();
