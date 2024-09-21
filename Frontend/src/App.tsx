0943b5b8930661f0243a3ba4e34ab4b0465144d1
Pablo <pablogutierrezvaldin@gmail.com>
2024-09-21 18:25:23 +0200

    Register mockup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./styles/pages/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ Frontend/src/pages/Register.tsx
@@ -0,0 +1,123 @@
+import { useState } from "react";
+
+export default function Register() {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [serverError, setServerError] = useState("");
+  const [showError, setShowError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    username: "",
+    confirmPassword: "",
+    password: "",
+  });
+
+  const onChangeInput = (e: any) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="text-white grid place-items-center px-6">
+          <h1 className="text-3xl text-center font-bold py-8 md:p-10">
+            Create an account
+          </h1>
+          {errorMessage && showError && (
+            <div className="w-full text-red-400 text-sm text-center p-3 mb-5 rounded border border-red-900 bg-red-950">
+              {errorMessage}
+            </div>
+          )}
+
+          {serverError && (
+            <div className="w-full text-red-400 text-sm text-center p-3 mb-5 rounded border border-red-900 bg-red-950">
+              {serverError}
+            </div>
+          )}
+
+          <form
+            action=""
+            className="grid grid-cols-12 gap-4 w-full max-w-md "
+            noValidate
+          >
+            <div className="col-span-12">
+              <label htmlFor="username" className="font-bold">
+                Username
+              </label>
+              <input
+                type="username"
+                id="username"
+                name="username"
+                placeholder="Username"
+                className="bg-gray-800 p-2 rounded w-full mt-2"
+                onChange={onChangeInput}
+              />
+            </div>
+            <div className="col-span-12">
+              <label htmlFor="email" className="font-bold">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Email"
+                className="bg-gray-800 p-2 rounded w-full mt-2"
+                onChange={onChangeInput}
+              />
+            </div>
+            <div className="col-span-12">
+              <div className="flex justify-between">
+                <label htmlFor="password" className="font-bold">
+                  Password
+                </label>
+              </div>
+
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                className="bg-gray-800 p-2 rounded w-full mt-2"
+                onChange={onChangeInput}
+              />
+            </div>
+            <div className="col-span-12">
+              <div className="flex justify-between">
+                <label htmlFor="password_confirmation" className="font-bold">
+                  Repeat password
+                </label>
+              </div>
+
+              <input
+                type="password"
+                id="password_confirmation"
+                name="password_confirmation"
+                placeholder="Repeat password"
+                className="bg-gray-800 p-2 rounded w-full mt-2"
+                onChange={onChangeInput}
+              />
+            </div>
+            <div className="col-span-12 flex justify-center p-0 py-2">
+              <button
+                type="submit"
+                className="bg-purple-500 p-2 rounded font-bold w-full"
+              >
+                Register
+              </button>
+            </div>
+            <div className="col-span-12">
+              <p className="text-center font-bold">
+                Already have an account? {""}
+                <a href="/login" className="text-purple-300 font-bold">
+                  Login to your account
+                </a>
+              </p>
+            </div>
+          </form>
+          <div className="mt-10">{isLoading && <p>Spinner...</p>}</div>
+        </div>
+      </div>
+    </>
+  );
+}
