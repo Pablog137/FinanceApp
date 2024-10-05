3e400c5f3dfe09d802e939f1371126899a838b3f
Pablo <pablogutierrezvaldin@gmail.com>
2024-10-05 14:08:20 +0200

    styles aside bar for mobile

--- Frontend/src/components/Shared/Aside/AsideMobile.tsx
+++ Frontend/src/components/Shared/Aside/AsideMobile.tsx
@@ -5,11 +5,12 @@ export default function AsideMobile() {
   const { isDarkMode, textColor } = useDarkMode();
 
   const bgColor = isDarkMode ? "dark:bg-gray-800" : "bg-white";
+  const stylesLightMode = !isDarkMode && "opacity-90";
 
   return (
     <aside
       id="logo-sidebar"
-      className={`${bgColor} fixed inset-x-0 bottom-0 w-full transition-transform sm:translate-x-0 flex-shrink-0 overflow-y-auto p-2 sm:hidden md:hidden`}
+      className={`${bgColor} fixed inset-x-0 bottom-0 w-full transition-transform sm:translate-x-0 flex-shrink-0 overflow-y-auto p-2 lg:hidden ${stylesLightMode}`}
       aria-label="Sidebar"
     >
       <ul className="font-medium list-none flex justify-center items-center">
